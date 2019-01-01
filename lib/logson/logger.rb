58304b3f4d871cc98c9bfb073fbdd01927d92114
Julien D <julien@unitylab.io>
2019-01-01 11:51:30 +0100

    update

--- lib/logson/logger.rb
+++ lib/logson/logger.rb
@@ -2,23 +2,25 @@ module Logson
   class Logger < ::Logger
     attr_accessor :hostname
 
+    FORMATTER = Proc.new do |severity, date, progname, msg|
+      date = date.utc.iso8601 if date.is_a?(Time)
+      data = {
+        '_severity' => severity, '_date' => date, '_source' => progname,
+        '_host' => hostname
+      }
+      if msg.is_a?(Hash)
+        data = data.merge(msg)
+      else
+        data['message'] = msg
+      end
+      JSON.dump(data) + "\n"
+    end.freeze
+
     def initialize(*args)
       super(*args)
 
       self.hostname = Socket.gethostname
-      self.formatter = proc do |severity, date, progname, msg|
-        date = date.utc.iso8601 if date.is_a?(Time)
-        data = {
-          '_severity' => severity, '_date' => date, '_source' => progname,
-          '_host' => hostname
-        }
-        if msg.is_a?(Hash)
-          data = data.merge(msg)
-        else
-          data['message'] = msg
-        end
-        JSON.dump(data)
-      end
+      self.formatter = FORMATTER
     end
 
     def source=(value)
